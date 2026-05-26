6d9d378d19fa980b5dbfab94c1d1f6101258fe8c
shresthaabhinav <shresthaabhinav5@gmail.com>
2026-05-26 09:53:26 +0545

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,8 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Abhinav",
-  description: "Abhinav",
+  title: "Abhinav Shrestha | Developer",
+  description: "I’m a MERN Stack Developer passionate about building modern, responsive, and user-friendly web applications. I specialize in MongoDB, Express.js, React.js, and Node.js to create full-stack solutions with clean UI, scalable backend architecture, and seamless user experiences. My portfolio showcases projects ranging from dynamic web applications to real-time systems, highlighting my skills in frontend development, backend APIs, database management, and problem-solving. I enjoy turning ideas into functional digital products and continuously learning new technologies to improve my development skills.",
 };
 
 export default function RootLayout({ children }) {
