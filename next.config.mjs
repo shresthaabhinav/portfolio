774efd7633f99f36d81407a0408505f6c1e78b38
Abhinav <shresthaabhinav5@gmail.com>
2026-09-15 13:22:39 +0545

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
-  trailingSlash: true, // optional but recommended for static hosting
-  images: {
-    unoptimized: true, // required for static export
-  },
+
 };
 
 export default nextConfig;
